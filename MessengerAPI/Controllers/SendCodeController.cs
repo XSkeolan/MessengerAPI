0cf7ada7fa4e92f69320869fc0d09220323fa82f
XSkeolan <fspiritlan@gmail.com>
2022-02-14 13:59:26 +0300

    edit gitignore

---
+++ MessengerAPI/Controllers/SendCodeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace MessengerAPI.Controllers
+{
+    [Route("api/sendCode")]
+    [ApiController]
+    public class SendCodeController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            MailAddress from = new MailAddress("somemail@gmail.com", "Tom");
+            MailAddress to = new MailAddress("somemail@yandex.ru");
+            MailMessage m = new MailMessage(from, to)
+            {
+                Subject = "Код подтверждения",
+                Body = "Вот код для подтверждения операции: "
+            };
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("somemail@gmail.com", "mypassword"),
+                EnableSsl = true
+            };
+            await smtp.SendMailAsync(m);
+            return Ok();
+        }
+    }
+}
